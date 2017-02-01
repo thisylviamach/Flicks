6da300ffe8b3677b0d8437e09dba56a1cd77cb24
Sylvia Mach <sylviamach@Sylvias-MacBook-Pro.local>
2017-01-31 23:32:07 -0800

    Required functionality done

--- MovieReview/MovieViewController.swift
+++ MovieReview/MovieViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import AFNetworking
+import MBProgressHUD
+
 
 class MovieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -17,6 +20,11 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        self.tableView.insertSubview(refreshControl, at: 0)
+        
         tableView.dataSource = self
         tableView.delegate = self
        
@@ -26,11 +34,14 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        
         let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
             if let data = data {
                 if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
                     print(dataDictionary)
                     
+                    MBProgressHUD.hide(for: self.view, animated: false)
                     self.movies = dataDictionary["results"] as? [NSDictionary]
                     self.tableView.reloadData()
                 }
@@ -42,20 +53,29 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         // Do any additional setup after loading the view.
     }
 
+    func refreshControlAction(_ refreshControl:UIRefreshControl){
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            self.tableView.reloadData()
+            refreshControl.endRefreshing()
+        }
+        task.resume()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        
-        
         if let movies = movies {
             return movies.count
         }else{
             return 0
         }
- 
     }
     
    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
@@ -64,9 +84,14 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
+        let baseUrl = "https://image.tmdb.org/t/p/w500"
+        let posterPath = movie["poster_path"] as! String
+    
+        let imageUrl = NSURL(string:baseUrl + posterPath)
     
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        cell.posterView.setImageWith(imageUrl as! URL)
     
     
         //cell.textLabel?.text = title
---
+++ Pods/Target Support Files/Pods-MovieReview/Pods-MovieReview-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MovieReview : NSObject
+@end
+@implementation PodsDummy_Pods_MovieReview
+@end
